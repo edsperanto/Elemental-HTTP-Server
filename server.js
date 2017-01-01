e4f1e536973aa556f6816785acc7a7620a2923c0
Edward Gao <egao@sent.com>
2016-12-31 19:26:23 -1000

    fix css display

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ let http = require('http');
 
 const PORT = process.env.PORT || 3000;
 
+let resourceMapping = {
+	'': './public/index.html',
+	'/': './public/index.html',
+	'/index.html': './public/index.html',
+	'/hydrogen.html': './public/hydrogen.html',
+	'/helium.html': './public/helium.html',
+	'/css/styles.css': './public/css/styles.css',
+	'/404.html': './public/404.html'
+}
+
 const server = http.createServer((req, res) => {
 
 	console.log(req.url);
@@ -17,16 +27,6 @@ const server = http.createServer((req, res) => {
 
 function getHandler(req, res) {
 	
-	let resourceMapping = {
-		'': './public/index.html',
-		'/': './public/index.html',
-		'/index.html': './public/index.html',
-		'/hydrogen.html': './public/hydrogen.html',
-		'/helium.html': './public/helium.html',
-		'/css/styles.css': './public/css/styles.css',
-		'/404.html': './public/404.html'
-	}
-
 	fs.readFile(resourceMapping[req.url] || resourceMapping['/404.html'], (err, content) => {
 		if(req.url.indexOf('css') > -1) {
 			res.setHeader('Content-Type', 'text/css');
@@ -75,9 +75,17 @@ function postHandler(req, res) {
   <p><a href="/">back</a></p>
 </body>
 </html>`, { defaultEncoding: 'utf8' });
+		updatePages(dataObj.elementName);
+	}
+
+	function updatePages(eleName) {
+		resourceMapping[`/${eleName}.html`] = `./public/${eleName}.html`;
+		console.log(resourceMapping);
 	}
 
 }
+
+
 
 server.listen(PORT, () => {
 	console.log('Server is listening on port', PORT);
