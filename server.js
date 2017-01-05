504fe3ac3e2d1210cf45667408bb9078af1462c8
Edward Gao <egao@sent.com>
2017-01-04 15:07:33 -1000

    mvp with resourceMapping

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-let fs = require('fs');
-let http = require('http');
+const fs = require('fs');
+const http = require('http');
+const qs = require('querystring');
+const PORT = process.env.PORT || 3000;
+const indexReset = require('./indexReset.js');
+const pageTemplate = require('./pageTemplate.js');
+
+// reset index.html every run
+fs.writeFile('./public/index.html', indexReset, { defaultEncoding: 'utf8' });
+
 let resourceMapping = {
 	'': './public/index.html',
 	'/': './public/index.html',
@@ -10,52 +18,36 @@ let resourceMapping = {
 	'/404.html': './public/404.html'
 }
 
-const PORT = process.env.PORT || 3000;
-const pageTemplate = require('./pageTemplate.js');
-const server = http.createServer((req, res) => {
+let server = http.createServer((req, res) => {
 	req.setEncoding('utf8');
 	getHandler(req, res);
 	postHandler(req, res);
 });
 
 function getHandler(req, res) {
-	fs.readFile(resourceMapping[req.url] || resourceMapping['/404.html'], (err, content) => {
-		if(req.url.indexOf('css') > -1) {
-			res.setHeader('Content-Type', 'text/css');
-		}else{
-			res.setHeader('Content-Type', 'text/html');
-		}
-		if(err) { res.statusCode = 404; }
-		res.write(content);
-		res.end();
-	});
+	if(req.method === 'GET') {
+		fs.readFile(resourceMapping[req.url] || resourceMapping['/404.html'], (err, content) => {
+			res.setHeader('Content-Type', (req.url.indexOf('css') > -1) ? ('text/css') : ('text/html'));
+			if(err) { res.statusCode = 404; }
+			res.write(content);
+			res.end();
+		});
+	}
 }
 
 function postHandler(req, res) {
-
 	if(req.url === '/elements' && req.method === 'POST') {
-		req.on('data', (chunk) => {
-			urlDecoder(chunk);
+		req.on('data', (chunk) => { 
+			let dataObj = qs.parse(chunk);
+			let newHTML = fs.writeFile(`./public/${dataObj.elementName}.html`, pageTemplate(dataObj), { defaultEncoding: 'utf8' });
+			resourceMapping[`/${dataObj.elementName}.html`] = `./public/${dataObj.elementName}.html`;
+			updatePage('add', dataObj.elementName);
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.write('{ "success": true }');
+			res.end();
 		});
 	}
-
-	function urlDecoder(chunk) {
-		let keyValueArr = chunk.split('&');
-		let newElementDataObj = {
-			elementName: keyValueArr[0].split('=')[1],
-			elementSymbol: keyValueArr[1].split('=')[1],
-			elementAtomicNumber: keyValueArr[2].split('=')[1],
-			elementDescription: keyValueArr[3].split('=')[1],
-		}
-		newElementPageGenFrom(newElementDataObj);
-	}
-
-	function newElementPageGenFrom(dataObj) {
-		let newHTML = fs.writeFile(`./public/${dataObj.elementName}.html`, pageTemplate(dataObj), { defaultEncoding: 'utf8' });
-		resourceMapping[`/${dataObj.elementName}.html`] = `./public/${dataObj.elementName}.html`;
-		updatePage('add', dataObj.elementName);
-	}
-
 }
 
 function updatePage(action, eleName) {
@@ -68,7 +60,7 @@ function updatePage(action, eleName) {
 			contentArr.splice(olIndex + 1, 0, '    <li>', `      <a href="./${eleName}.html">${eleName}</a>`, '    </li>');
 			fs.writeFile('./public/index.html', contentArr.join('\n'), { defaultEncoding: 'utf8' });
 		}else if(action === 'remove') {
-			
+
 		}
 	});
 }
